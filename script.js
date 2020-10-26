5bf9dde55bd669157692cd42e9b4e9d3656dcaee
Ann Markham <ann.markham@outlook.com>
2020-10-25 22:06:28 -0700

    added save and local storage features

--- script.js
+++ script.js
@@ -9,7 +9,14 @@ var today = moment().format("dddd, MMMM Do - YYYY [time is] h:m a ");
 $("#currentDay").text(today);
 
 
+schedule = JSON.parse(localStorage.getItem("schedule"));
+if (!schedule) {
+  schedule = [];
+};
+
+
 for (i = 0; i < 9; i++) {
+
   var timeRow = $("<div>").addClass("row row" + i);
   $(".container").append(timeRow);
 
@@ -18,19 +25,47 @@ for (i = 0; i < 9; i++) {
   var textCol = $("<textArea>").addClass("col col-9").attr("id", "text" + i);
 
   var saveCol = $("<div>").addClass("col col-1 saveBtn").attr("id", "save" + i);
+
   $(".row" + i).append(hourCol, textCol, saveCol);
 
   var saveIcon = $("<i>").addClass("far fa-save fa-4x");
   $("#save" + i).append(saveIcon);
 
+
+
   //color based on time
   var hourText = moment(9 + i, "HH").format("ha");
   $("#hour" + i).text(hourText);
-  if (moment(9 + i, "HH").isSame(moment(), "hour")) {
-    $("#text" + i).addClass("present");
-  } else if (moment(9 + i, "HH").isBefore(moment())) {
+
+  if (moment(9 + i, "HH").isBefore(moment())) {
     $("#text" + i).addClass("past");
-  } else if (moment(9 + i, "HH").isAfter(moment())) {
+  }
+  else if (moment(9 + i, "HH").isAfter(moment())) {
     $("#text" + i).addClass("future");
-  };
+  }
+  else {
+    $("#text" + i).addClass("present");
+  }
+
+  $("#text" + i).text(schedule[i]);
 };
+
+
+//save button function
+$(".saveBtn").on("click", function () {
+  schedule = []
+  for (i = 0; i < 9; i++) {
+    schedule.push($("#text" + i).val());
+  }
+
+  localStorage.setItem("schedule", JSON.stringify(schedule));
+});
+
+// save function
+var saveAppts = function () {
+  schedule = []
+  for (i = 0; i < 9; i++) {
+    schedule.push($("#text" + i).val());
+  }
+  localStorage.setItem("schedule", JSON.stringify(schedule));
+}
